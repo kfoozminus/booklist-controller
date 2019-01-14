078bd10c191f2339f2e852dbce89a85c3c3f28ca
kfoozminus <kfoozminus@gmail.com>
2019-01-14 20:15:25 +0600

    Move workqueue

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"path/filepath"
+
+	"github.com/kfoozminus/booklist-controller/pkg/client/clientset/versioned"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := versioned.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+}
